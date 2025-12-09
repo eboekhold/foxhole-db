6ce33364a36af69c359bd44d791552b762ca9cfb
Eva <eva.boekhold@gmail.com>
2025-12-09 03:38:59 +0100

    Add endpoint for refinables

--- main.py
+++ main.py
@@ -4,26 +4,41 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
+import json
+
 app = FastAPI()
 
 
-class Item(BaseModel):
-  name: str
-  price: float
-  is_offer: Union[bool, None] = None
+# Open access to raw data sources
+app.mount("/public/", StaticFiles(directory="public"), name="public")
+app.mount("/raw_data/", StaticFiles(directory="data/json2"), name="raw_data")
 
-
-app.mount("/", StaticFiles(directory="public"), name="public")
-
+# Health
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
 
+# ============================
+# == Processed Data Sources ==
+# ============================
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-  return {"item_id": item_id, "q": q}
+# Refinables
+@app.get("/refinables")
+def read_refinables():
+  with open('data/json2/items_refinable.json') as fp:
+    data = json.load(fp)
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#   return {"item_name": item.name, "item_id": item_id}
+    output_data = {
+      recipe['Refined Item Name']: {
+        'Input Resource': recipe['Source Item Name'],
+        'Input Amount': 1 / recipe['YieldModifier'],
+        'Output Resource': recipe['Refined Item Name'],
+        'Output Amount': 1,
+        'Time': ((60.0 / recipe['YieldModifier']) / recipe['SpeedModifier']) * 1000,
+        'Max Refined Item Count': recipe['MaxRefinedItemCount']
+      }
+      for _, recipe in data.items()
+    }
+
+    return output_data
+
